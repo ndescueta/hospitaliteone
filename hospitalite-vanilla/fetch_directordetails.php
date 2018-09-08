fd40f603790fb828f6d168ac90f312478fea4561
Nins <ndescueta@gmail.com>
2018-09-08 08:06:47 +0800

    fetch director details

--- hospitalite-vanilla/fetch_directordetails.php
+++ hospitalite-vanilla/fetch_directordetails.php
@@ -2,9 +2,9 @@
 include("connections.php");
 
 if($_POST["directorid"]){
-  $eventid = $_POST["directorid"];
+  $directorid = $_POST["directorid"];
 
-  $fetch_directordetails = mysqli_query($connections, " SELECT intDirectorId, strDirectorFirstName, strDirectorMiddleName, strDirectorLastName, strDirectorContact FROM tbldirector");
+  $fetch_directordetails = mysqli_query($connections, " SELECT intDirectorId, strDirectorFirstName, strDirectorMiddleName, strDirectorLastName, strDirectorContact FROM tbldirector WHERE intDirectorID = $directorid");
 
   $row = mysqli_fetch_assoc($fetch_directordetails);
 
